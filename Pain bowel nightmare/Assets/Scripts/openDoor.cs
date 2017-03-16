a903e14f49d5101494075c4cffd541753335a160
010011100010 <ll3305@nyu.edu>
2017-03-15 22:25:45 -0400

    Working on door hinges

---
+++ Pain bowel nightmare/Assets/Scripts/openDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour {
+    public GameObject hinge;
+    public GameObject openDoorPrompt;
+    public Collider promptCollider;
+    public KeyCode openDoorKey = KeyCode.F;
+	// Use this for initialization
+	void Start () {
+        openDoorPrompt.SetActive(false);
+	}
+
+    void OnTriggerEnter (Collider promptCollider)
+    {
+        openDoorPrompt.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
