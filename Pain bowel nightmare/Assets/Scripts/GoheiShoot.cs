85a289c38f7df1957e8fb1f69f992424cffc3773
010011100010 <ll3305@nyu.edu>
2017-03-17 14:33:47 -0400

    More feature, more rage
    The Gohei is equippable now;
    When equipped, the Gohei is able to shoot now;
    When eqipped, the Gohei still collides with stuff as its wish depite
    being explictly told not so in the script;
    When shot, the projectiles will ramdomly decide whichever object they
    want to collide with and only collide with said objects;
    When colliding, projectiles will ramdomly decide wherever they want to
    scratch sparks;
    Projectiles will explode into fireworks.

---
+++ Pain bowel nightmare/Assets/Scripts/GoheiShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoheiShoot : MonoBehaviour
+{
+    public GameObject projectile;
+    public Vector3 emitter;
+    public float bulletForce = 10f;
+    public AudioClip pewSound;
+    public AudioSource mySource;
+
+    // Use this for initialization
+    void Start()
+    {
+        mySource.clip = pewSound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        emitter = GameObject.Find("BulletShooter").transform.position;
+
+        if (GameObject.Find("Gohei rod").GetComponent<equipA2>().equipped == true && Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(projectile, emitter, Quaternion.identity)
+               as GameObject;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.up * bulletForce);
+            mySource.PlayOneShot(pewSound);
+        }
+
+
+    }
+}
---
+++ Pain bowel nightmare/Assets/Scripts/bulletSpark.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletSpark : MonoBehaviour {
+    public GameObject explosion;
+    public GameObject spark;
+    public Collider bulletCollider;
+    public float deathTimer = 3f;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("StarryExplosion", deathTimer);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Destroy(gameObject, deathTimer);
+	}
+
+    void OnCollisionEnter (Collision bulletCollider)
+    {
+        Instantiate(spark, bulletCollider.gameObject.transform.position, bulletCollider.gameObject.transform.rotation);
+    }
+
+    void StarryExplosion ()
+    {
+        Instantiate(explosion, bulletCollider.gameObject.transform.position, Quaternion.identity);
+    }
+}
