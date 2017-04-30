4b2760fe10fbcbaf7c24af7e187b8a3602d8e687
010011100010 <ll3305@nyu.edu>
2017-04-30 19:18:42 -0400

    Huge asset update

---
+++ Pain bowel nightmare/Assets/Scripts/slideDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class slideDoor : MonoBehaviour {
+    private Vector3 slideInPos;
+    private Collider doorTrigger;
+	// Use this for initialization
+	void Start () {
+        slideInPos = transform.position + new Vector3(0, 0, 3);
+        doorTrigger = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter()
+    {
+        print("Open door");
+        gameObject.transform.DOMove(slideInPos, 0.5f);
+        Invoke("Close", 2);
+    }
+
+    void Close()
+    {
+        gameObject.transform.DOMove(slideInPos - new Vector3(0, 0, 3), 0.5f);
+    }
+}
