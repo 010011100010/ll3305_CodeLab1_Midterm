420f755fa9a8798c254e43f71cacf92072ac93c8
010011100010 <ll3305@nyu.edu>
2017-04-30 23:58:30 -0400

    Changed explosion prefabs and wrote some new scripts

---
+++ Pain bowel nightmare/Assets/Scripts/baseTarget.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baseTarget : MonoBehaviour {
+
+    protected string name = "Basic Target";
+    public float moveSpeed = 2f;
+    private Rigidbody rb;
+    public GameObject[] explosions;
+
+    float _health = 100f;
+    public float health
+    {
+        get { return _health; }
+        set
+        {
+            //Debug.Log("Current health: " + value);
+
+            if (value <= 0f)
+            {
+                Die();
+            }
+
+            else
+                _health = value;
+        }
+    }
+
+    public virtual void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public virtual void Update ()
+    {
+        rb.velocity = new Vector3(moveSpeed * (Mathf.PerlinNoise(Time.timeSinceLevelLoad, 0) - 0.5f) * 2, moveSpeed * (Mathf.PerlinNoise(0, Time.timeSinceLevelLoad) - 0.5f) * 2, 0);
+    }
+
+    protected virtual void Die()
+    {
+        Instantiate(explosions[Random.Range(0, explosions.Length)], transform.position, Quaternion.identity);
+    }
+}
---
+++ Pain bowel nightmare/Assets/Scripts/explosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour {
+    private AudioSource source;
+    public AudioClip[] explosiveClips;
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        source.PlayOneShot(explosiveClips[Random.Range(0, explosiveClips.Length)]);
+        Destroy(gameObject, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
