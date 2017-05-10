0872009783d9877dd34d325b6707d31b5e83e019
010011100010 <ll3305@nyu.edu>
2017-05-10 00:49:53 -0400

    Added fecal generator

---
+++ Pain bowel nightmare/Assets/Scripts/defecate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class defecate : MonoBehaviour {
+    public GameObject fecalParent;
+    public float speed = 20f;
+    public float spreadFactor = 0.1f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Quaternion pelletRotation = transform.rotation;
+            pelletRotation.x += Random.Range(-spreadFactor, spreadFactor);
+            pelletRotation.y += Random.Range(-spreadFactor, spreadFactor);
+            pelletRotation.z += Random.Range(-spreadFactor, spreadFactor);
+            GameObject fecalClump = Instantiate(fecalParent, transform.position, pelletRotation) as GameObject;
+            Rigidbody fecalRb = fecalClump.GetComponent<Rigidbody>();
+            fecalRb.velocity = transform.up * speed;
+            print("defecating");
+        }
+	}
+}
